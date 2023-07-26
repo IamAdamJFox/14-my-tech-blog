9f76265512e549dee152e40b351cb46a26179109
adamjfox <adamjeffersonfox@gmail.com>
2023-07-25 21:52:14 -0700

    passwords fixed

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,5 +1,6 @@
 // Importing the User model from "../models" directory
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 // Array of dummy user data to be inserted into the database
 const userData = [
@@ -19,8 +20,17 @@ const userData = [
 // The seedUsers function to insert the user data into the database
 const seedUsers = async () => {
   try {
+    // Hash the passwords before inserting into the database
+    const hashedUserData = await Promise.all(
+      userData.map(async (user) => {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        return { ...user, password: hashedPassword };
+      })
+    );
+
     // Using Sequelize's bulkCreate to insert multiple users into the database
-    await User.bulkCreate(userData);
+    await User.bulkCreate(hashedUserData);
 
     console.log("User data seeded successfully!");
   } catch (error) {
